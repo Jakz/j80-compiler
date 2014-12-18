59d10fd180a044fc9b3fb3266678f2c857ca2265
Jack <jack@pixbits.com>
2014-12-18 23:18:17 +0100

    minor issues

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -44,30 +44,6 @@ void J80Assembler::placeLabel(const std::string& label)
   labels[label] = position;
 }
 
-bool J80Assembler::solveJumps()
-{
-  for (auto &jump : jumps)
-  {
-    unordered_map<std::string, u16>::iterator it = labels.find(jump.second);
-    
-    if (it != labels.end())
-    {
-      u16 realAddress = codeSegment.offset + it->second;
-
-      printf("Jump to %s at address %04X - %p\n", jump.second.c_str(), it->second, &jump.first);
-      codeSegment.data[jump.first+codeSegment.offset+2] = realAddress & 0xFF;
-      codeSegment.data[jump.first+codeSegment.offset+1] = (realAddress >> 8) & 0xFF;
-    }
-    else
-    {
-      printf("Label %s unresolved!\n", jump.second.c_str());
-      return false;
-    }
-  }
-  
-  return true;
-}
-
 void J80Assembler::error (const Assembler::location& l, const std::string& m)
 {
   cerr << "Assembler error at " << file << ":" << l.begin.line << "," << l.begin.column << " : " << m << endl;
@@ -207,6 +183,30 @@ void J80Assembler::buildCodeSegment()
     totalSize += it->length;
     ++it;
   }
+}
+
+bool J80Assembler::solveJumps()
+{
+  for (auto &jump : jumps)
+  {
+    unordered_map<std::string, u16>::iterator it = labels.find(jump.second);
+    
+    if (it != labels.end())
+    {
+      u16 realAddress = codeSegment.offset + it->second;
+
+      printf("Jump to %s at address %04X - %p\n", jump.second.c_str(), it->second, &jump.first);
+      codeSegment.data[jump.first+codeSegment.offset+2] = realAddress & 0xFF;
+      codeSegment.data[jump.first+codeSegment.offset+1] = (realAddress >> 8) & 0xFF;
+    }
+    else
+    {
+      printf("Label %s unresolved!\n", jump.second.c_str());
+      return false;
+    }
+  }
+  
+  return true;
 }
 
 void J80Assembler::solveDataReferences()
