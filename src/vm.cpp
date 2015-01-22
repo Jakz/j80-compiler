cff2d8602062d13150dc9f1ba321990a2d1a1fe5
Jack <jack@pixbits.com>
2015-01-22 06:10:21 +0100

    added almost all simple instruction to J80 VM

--- src/vm.cpp
+++ src/vm.cpp
@@ -30,9 +30,26 @@ u16& VM::reg16(Reg r)
   }
 }
 
+bool VM::isConditionTrue(JumpCondition condition) const
+{
+  u8 flags = regs.FLAGS;
+  
+  switch (condition) {
+    case COND_CARRY: return flags & CARRY;
+    case COND_NCARRY: return !(flags & CARRY);
+    case COND_ZERO: return flags & ZERO;
+    case COND_NZERO: return !(flags & ZERO);
+    case COND_OVERFLOW: return flags & OVERFLOW;
+    case COND_NOVERFLOW: return !(flags & OVERFLOW);
+    case COND_SIGN: return flags & SIGN;
+    case COND_NSIGN: return !(flags & SIGN);
+    case COND_UNCOND: return true;
+  }
+}
+
 void VM::executeInstruction()
 {
-  u8 *d = code;
+  u8 *d = &code[regs.PC];
   
   Opcode opcode = static_cast<Opcode>(d[0]>>3);
   Reg reg1 = static_cast<Reg>(d[0] & 0b111);
@@ -40,7 +57,7 @@ void VM::executeInstruction()
   Reg reg3 = static_cast<Reg>(d[2] >> 5);
   u8 unsigned8 = (u8)d[2];
   s8 signed8 = (s8)d[2];
-  u16 short1 = d[1] | (d[2]<<8);
+  u16 short1 = d[2] | (d[1]<<8);
   u16 short2 = d[2] | (d[3]<<8);
   AluOp alu = static_cast<AluOp>(d[1] & 0b11111);
   JumpCondition cond = static_cast<JumpCondition>(d[0] & 0b1111);
@@ -50,7 +67,44 @@ void VM::executeInstruction()
     // R8 <- R8, R16 <- R16, RSH/LSH R8, RSH/LSH R16
     case OPCODE_LD_RSH_LSH:
     {
-      // TODO
+      switch (alu) {
+      }
+    }
+      
+    case OPCODE_ALU_NN:
+    {
+      
+      break;
+    }
+      
+    case OPCODE_ALU_NNNN:
+    {
+      
+      break;
+    }
+      
+    case OPCODE_ALU_REG:
+    {
+      
+      break;
+    }
+      
+    case OPCODE_CMP_NN:
+    {
+      
+      break;
+    }
+      
+    case OPCODE_CMP_NNNN:
+    {
+      
+      break;
+    }
+      
+    case OPCODE_CMP_REG:
+    {
+      
+      break;
     }
       
       // R <- NN
@@ -111,18 +165,139 @@ void VM::executeInstruction()
       break;
     }
       
+    case OPCODE_JMPC_NNNN:
     case OPCODE_JMP_NNNN:
     {
-      u16 address = short1;
-      regs.PC = address;
+      if (isConditionTrue(cond))
+      {
+        u16 address = short1;
+        regs.PC = address;
+      }
       break;
     }
       
+    case OPCODE_JMPC_PP:
     case OPCODE_JMP_PP:
     {
-      u16 address = reg16(reg2);
-      regs.PC = address;
+      if (isConditionTrue(cond))
+      {
+        u16 address = reg16(reg2);
+        regs.PC = address;
+      }
       break;
     }
+      
+    case OPCODE_NOP:
+    {
+      break;
+    }
+      
+    case OPCODE_PUSH:
+    {
+      u8& r = reg8(reg1);
+      u16& sp = reg16(REG_SP);
+      --sp;
+      ramWrite(sp, r);
+      break;
+    }
+      
+    case OPCODE_PUSH16:
+    {
+      u16& r = reg16(reg1);
+      u16& sp = reg16(REG_SP);
+      --sp;
+      ramWrite(sp, r & 0xFF);
+      --sp;
+      ramWrite(sp, (r >> 8) & 0xFF);
+      break;
+    }
+      
+    case OPCODE_POP:
+    {
+      u8& r = reg8(reg1);
+      u16& sp = reg16(REG_SP);
+      r = ramRead(sp);
+      ++sp;
+      break;
+    }
+      
+    case OPCODE_POP16:
+    {
+      u16& r = reg16(reg1);
+      u16& sp = reg16(REG_SP);
+      u8 high = ramRead(sp);
+      ++sp;
+      u8 low = ramRead(sp);
+      ++sp;
+      r = (high << 8)| low;
+      break;
+    }
+      
+    case OPCODE_RET:
+    case OPCODE_RETC:
+    {
+      if (isConditionTrue(cond))
+      {
+        u16& sp = reg16(REG_SP);
+        ++sp;
+        u8 high = ramRead(sp);
+        ++sp;
+        u8 low = ramRead(sp);
+        regs.PC = (high << 8) | low;
+      }
+      break;
+    }
+      
+    case OPCODE_CALL:
+    case OPCODE_CALLC:
+    {
+      if (isConditionTrue(cond))
+      {
+        u16& sp = reg16(REG_SP);
+        --sp;
+        ramWrite(sp, regs.PC & 0xFF);
+        --sp;
+        ramWrite(sp, (regs.PC >> 8) & 0xFF);
+        regs.PC = short1;
+      }
+      break;
+    }
+      
+    case OPCODE_LF:
+    {
+      u8& r = reg8(reg1);
+      regs.FLAGS = 0x0F & r;
+      break;
+    }
+      
+    case OPCODE_SF:
+    {
+      u8& r = reg8(reg1);
+      r = 0x0F & regs.FLAGS;
+      break;
+    }
+      
+    case OPCODE_EI:
+    {
+      interruptEnabled = true;
+      break;
+    }
+      
+    case OPCODE_DI:
+    {
+      interruptEnabled = false;
+      break;
+    }
+      
+    case OPCODE_SEXT:
+    {
+      u8& r = reg8(reg1);
+      u8& h = reg8(static_cast<Reg>(reg1 | 0x100));
+      h = r & 0x80 ? 0xFF : 0x00;
+      break;
+    }
+      
+      
+    
   }
 }
--- src/vm.h
+++ src/vm.h
@@ -58,16 +58,39 @@ struct Regs
   u16 PC;
 };
 
+enum Flag : u8
+{
+  CARRY = 0x01,
+  ZERO = 0x02,
+  SIGN = 0x04,
+  OVERFLOW = 0x08
+};
+
 class VM
 {
   private:
     Regs regs;
     u8 *code;
     u8 *ram;
+    bool interruptEnabled;
+  
+    void add8(u8& op1, u8& op2, u8& dest, bool flags = true);
+    void adc8(u8& op1, u8& op2, u8& dest, bool flags = true);
+    void sub8(u8& op1, u8& op2, u8& dest, bool flags = true);
+    void sbc8(u8& op1, u8& op2, u8& dest, bool flags = true);
+    void add16(u16& op1, u16& op2, u16& dest, bool flags = true);
+    void adc16(u16& op1, u16& op2, u16& dest, bool flags = true);
+    void sub16(u16& op1, u16& op2, u16& dest, bool flags = true);
+    void sbc16(u16& op1, u16& op2, u16& dest, bool flags = true);
+  
+    inline void setFlag(Flag flag) { regs.FLAGS |= flag; }
+    inline void unsetFlag(Flag flag) { regs.FLAGS &= ~flag; }
   
   public:
     void reset() { regs.PC = 0; }
     void executeInstruction();
+  
+    bool isConditionTrue(JumpCondition condition) const;
   
     void ramWrite(u16 address, u8 value) { ram[address] = value; }
     u8 ramRead(u16 address) { return ram[address]; }
